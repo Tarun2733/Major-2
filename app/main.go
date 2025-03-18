0088227ac3a14fdd8aa0c9bbd0f2cde80eeccb59
Tarun <taruntiwari.4113@gmail.com>
2025-03-18 23:03:52 +0530

    The Craziest Commit

--- app/main.go
+++ app/main.go
@@ -1,93 +1,100 @@
 package main
+
 import (
 	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
+
+// Entry point of the Kafka clone
 func main() {
+	// Start listening on TCP port 9092 (default Kafka port)
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
+	fmt.Println("Kafka clone is running on port 9092...")
+
 	for {
-		conn, err := l.Accept()
+		conn, err := l.Accept() // Accept new connections
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn) // Handle each connection concurrently
 	}
 }
+
+// Function to handle client connections
 func handleConnection(conn net.Conn) {
+	defer conn.Close() // Ensure the connection is closed when done
 	for {
 		req := make([]byte, 1024)
-		conn.Read(req)
-		res := hadleRequest(req)
+		n, err := conn.Read(req)
+		if err != nil {
+			fmt.Println("Connection closed by client")
+			return
+		}
+
+		// Process request and generate response
+		res := handleRequest(req[:n])
 		conn.Write(res)
 	}
 }
+
+// Struct to store Kafka request headers
 type RequestHeader struct {
 	requestApiKey     int16
 	requestApiVersion int16
 	correlationId     int32
-	clientId          []byte
-	taggedFields      []byte
 }
+
+// Function to parse headers from Kafka-like requests
 func parseHeaders(req []byte) *RequestHeader {
 	return &RequestHeader{
-		requestApiKey:     int16(binary.BigEndian.Uint16(req[4:8])),
+		requestApiKey:     int16(binary.BigEndian.Uint16(req[4:6])),
 		requestApiVersion: int16(binary.BigEndian.Uint16(req[6:8])),
 		correlationId:     int32(binary.BigEndian.Uint32(req[8:12])),
 	}
 }
+
+// Function to create Kafka-like responses
 func makeResponse(reqHeaders *RequestHeader) []byte {
 	res := make([]byte, 8)
 	binary.BigEndian.PutUint32(res[4:8], uint32(reqHeaders.correlationId))
-	// check api version
+
+	// If API version is unsupported, return error
 	if reqHeaders.requestApiVersion < 0 || reqHeaders.requestApiVersion > 4 {
-		res = append(res, 0, 35)
+		res = append(res, 0, 35) // Error code 35 (unsupported version)
 		setMessageSize(res)
 		return res
 	}
-	// ApiVersions
+
+	// Handle API key 18 (GetApiVersions request)
 	if reqHeaders.requestApiKey == 18 {
-		// https://forum.codecrafters.io/t/question-about-handle-apiversions-requests-stage/1743/4
-		// ApiVersions Response (Version: CodeCrafters) =>
-		//		error_code num_of_api_keys [api_keys] throttle_time_ms TAG_BUFFER
-		//
-		// error_code => INT16
-		// num_of_api_keys => INT8
-		// api_keys => api_key min_version max_version
-		//   api_key => INT16
-		// 	 min_version => INT16
-		//   max_version => INT16
-		// _tagged_fields
-		// throttle_time_ms => INT32
-		// _tagged_fields
 		res = append(res,
-			0, 0, // error_code no error
-			3,     // num of api keys + 1
-			0, 18, // api key GetApiVersions
-			0, 3, // min version
-			0, 4, // max version
-			0,     // tagged fields
-			0, 75, // api key DescribeTopicPartitions
-			0, 0, // min version
-			0, 0, // max version
-			0,          // tagged fields
-			0, 0, 0, 0, // throttle time
-			0, //tagged fields
+			0, 0, // No error
+			3,    // Number of API keys
+			0, 18, 0, 3, 0, 4, // API key 18 (GetApiVersions), versions 3-4
+			0, 75, 0, 0, 0, 0, // API key 75 (DescribeTopicPartitions)
+			0, 0, 0, 0, // Throttle time
+			0, // Tagged fields
 		)
 	}
+
 	setMessageSize(res)
 	return res
 }
+
+// Function to set the message size at the beginning of the response
 func setMessageSize(res []byte) {
 	binary.BigEndian.PutUint32(res[0:4], uint32(len(res)-4))
 }
-func hadleRequest(req []byte) []byte {
+
+// Function to process client requests
+func handleRequest(req []byte) []byte {
 	headers := parseHeaders(req)
 	return makeResponse(headers)
 }
